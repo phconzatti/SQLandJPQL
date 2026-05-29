919d4eddf7746b821c6f8432ed94201baa08df20
phconzatti <phconzatti@hotmail.com>
2026-05-29 10:55:33 -0300

    uri 2609

--- bdsconsultas-main/uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
+++ bdsconsultas-main/uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
@@ -1,11 +1,16 @@
 package com.devsuperior.uri2609;
 
+import com.devsuperior.uri2609.dto.CategorySumDTO;
+import com.devsuperior.uri2609.projections.CategorySumProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2609.repositories.CategoryRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Uri2609Application implements CommandLineRunner {
@@ -20,5 +25,17 @@ public class Uri2609Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+		List<CategorySumProjection> list = repository.search1();
+		List<CategorySumDTO> result1 = list.stream().map(x -> new CategorySumDTO(x)).collect(Collectors.toList());
+
+		for (CategorySumDTO obj : result1) {
+			System.out.println(obj);
+		}
+
+		List<CategorySumDTO> result2 = repository.search2();
+
+		for (CategorySumDTO obj : result1) {
+			System.out.println(obj);
+		}
 	}
 }
